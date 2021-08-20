6d152dc97a08d1b9ca2cc439d55972a139eb0b96
dubnyaka <dubnyaka@gmail.com>
2021-08-20 16:17:59 +0300

    improve links, buttons. Finished reviews and add Flickity slider

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 $(function(){
+    $(document).ready(function(){       
+        $('.carousel').flickity({
+            // options
+            cellAlign: 'center',
+            initialIndex: 1,
+            contain: true,
+            pageDots: false,
+            prevNextButtons: false,
+            autoPlay: true,
+          });
+    });
 
 });
